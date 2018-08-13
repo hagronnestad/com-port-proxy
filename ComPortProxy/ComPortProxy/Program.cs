30bd57690dee87aec62e882a02e40bc2f28d63e2
Hein Andre Grønnestad <post@heinandre.no>
2018-08-14 00:29:36 +0200

    PoC

---
+++ ComPortProxy/ComPortProxy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+
+namespace ComPortProxy {
+
+    internal class Program {
+
+        public static SerialPort spESP = new SerialPort("COM4", 115200);
+        public static SerialPort spNextion = new SerialPort("COM3", 9600);
+
+        private static void Main(string[] args) {
+            spESP.Open();
+            spNextion.Open();
+
+            while (true) {
+                var b = (byte) spESP.ReadByte();
+                spNextion.Write(new byte[] { b }, 0, 1);
+
+                Console.Write(Encoding.UTF8.GetString(new byte[] { b }));
+            }
+        }
+
+    }
+}
